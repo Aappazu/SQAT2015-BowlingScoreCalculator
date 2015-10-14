211f32d6c764401dd9716d60574a04ed7a2255eb
somename <someemail>
2015-10-14 15:53:28 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/Frame.java
+++ src/org/unioulu/tol/sqat/bsc/Frame.java
@@ -86,6 +86,12 @@ public class Frame {
 				if(this.checkIfScoreValid() == true){
 					return true;
 				}
+				else{
+					return false;
+				}
+			}
+			else{
+				return false;
 			}
 		}
 		else{
