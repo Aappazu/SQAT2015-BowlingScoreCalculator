94fe408fb05f442babdb867972f9cf41a5d42ede
somename <someemail>
2015-10-14 15:03:03 +0300

    besouro automatic message

--- src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
+++ src/org/unioulu/tol/sqat/bsc/tests/TestBowlingScoreCalculator.java
@@ -3,7 +3,7 @@ package org.unioulu.tol.sqat.bsc.tests;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-import org.unioulu.tol.sqat.bsc.Frame;
+import org.unioulu.tol.sqat.bsc.*;
 
 public class TestBowlingScoreCalculator {
 
